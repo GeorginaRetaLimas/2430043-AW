5db16052e2504c5a32f6001044a2b8666a7c4013
GEORGINA RETA LIMAS <2430043@upv.edu.mx>
2025-10-27 20:14:15 -0600

    Terminado

--- Practica_8/script.js
+++ Practica_8/script.js
@@ -9,12 +9,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function mostrarCantidadMaterias(){
 
-    let cantidad = document.getElementById("cantidad_alumnos").value;
+    let cantidad = document.getElementById("cantidad_materias").value;
     console.log(cantidad);
 
     if(cantidad <= 0){
         Swal.fire('Error cantidad minima', 'La cantidad mínima de materias es 1','warning');
-        document.getElementById("cantidad_alumnos") = "";
+        document.getElementById("cantidad_materias").value = "";
+        return;
+    } else if(cantidad > 20){
+        Swal.fire('Error cantidad maxima', 'La cantidad maxima es de materias es 20','warning');
+        document.getElementById("cantidad_materias").value = "";
         return;
     }
 
@@ -28,28 +32,28 @@ function mostrarCantidadMaterias(){
             <div class="card overflow-auto mb-3">
                 <div class="card-body">
                     <h3 id="encabezado">Materia ${i+1}</h3>
-                    <form id="formUnidades">
-                        <label for="u1">Unidad 1</label>
-                        <input type="number" id="u1" class="form-control">
+                    <div>
+                        <label for="u1_${i}">Unidad 1</label>
+                        <input type="number" id="u1_${i}" class="form-control">
 
-                        <label for="u2">Unidad 2</label>
-                        <input type="number" id="u2" class="form-control">
+                        <label for="u2_${i}">Unidad 2</label>
+                        <input type="number" id="u2_${i}" class="form-control">
 
-                        <label for="u3">Unidad 3</label>
-                        <input type="number" id="u3" class="form-control">
+                        <label for="u3_${i}">Unidad 3</label>
+                        <input type="number" id="u3_${i}" class="form-control">
 
-                        <label for="u4">Unidad 4</label>
-                        <input type="number" id="u4" class="form-control">
+                        <label for="u4_${i}">Unidad 4</label>
+                        <input type="number" id="u4_${i}" class="form-control">
 
                         <button class="btn btn-primary rounded-pill px-3 mt-3" onclick="calificar('${i}')">
                             Calificar
                         </button>
-                    </form>
+                    </div>
                 </div>
 
                 <div class="card-footer">
-                    <h4 id="respuestaGrande">Respuesta</h4>
-                    <p id="respuestaChiquita">Aqui se vera tu respuesta</p>
+                    <h4 id="respuestaGrande_${i}">Respuesta</h4>
+                    <p id="respuestaChiquita_${i}">Aqui se vera tu respuesta</p>
                 </div>
             </div>
         `;
@@ -61,26 +65,43 @@ function mostrarCantidadMaterias(){
 };
 
 function calificar(n){
-    const u1 = document.getElementById('u1').value;
-    const u2 = document.getElementById('u2').value;
-    const u3 = document.getElementById('u3').value;
-    const u4 = document.getElementById('u4').value;
+    event.preventDefault();
+    event.stopPropagation();
 
-    if(u1 <= 0 && u2 <= 0 && u3 <= 0 && u4 <= 0){
-        Swal.fire('Error cantidad minima', 'La cantidad mínima es 0','warning');
+    let u1 = parseFloat(document.getElementById(`u1_${n}`).value);
+    let u2 = parseFloat(document.getElementById(`u2_${n}`).value);
+    let u3 = parseFloat(document.getElementById(`u3_${n}`).value);
+    let u4 = parseFloat(document.getElementById(`u4_${n}`).value);
+
+    console.log(u1, " ", u2, " ", u3, " ", u4);
+
+    if(u1 < 0 || u2 < 0 || u3 < 0 || u4 < 0){
+        Swal.fire('Error cantidad minima', 'La cantidad mínima es 0','error');
+        return;
     }
 
-    if(u1 > 100 && u2 > 100 && u3 > 100 && u4 > 100){
-        Swal.fire('Error cantidad maxima', 'La cantidad maxima es 10','success');
+    if(u1 > 100 || u2 > 100 || u3 > 100 || u4 > 100){
+        Swal.fire('Error cantidad maxima', 'La cantidad maxima es 10','error');
+        return;
     }
 
     let promedio = (u1 + u2 + u3 + u4)/4;
+    let respuestaGrande = document.getElementById(`respuestaGrande_${n}`);
+    let respuestaChiquita = document.getElementById(`respuestaChiquita_${n}`);
 
-    if(promedio >= 7){
-        document.getElementById('respuestaGrande') = "APROBADO";
-        let mensaje = "Aprobado " + promedio;
+    if(u1 >= 70 && u2 >= 70 && u3 >= 70 && u4 >= 70){
+        if(promedio >= 70){
+            respuestaGrande.textContent = "APROBADO";
+            respuestaGrande.className = "text-success";
+            respuestaChiquita.textContent = "Aprobado con promedio: " + promedio.toFixed(2);
+        } else {
+            respuestaGrande.textContent = "NO APROBADO";
+            respuestaGrande.className = "text-danger";
+            respuestaChiquita.textContent = "No aprobado con promedio: " + promedio.toFixed(2);
+        }
     } else {
-        document.getElementById('respuestaGrande') = "REPROBADO";
-        let mensaje = "Reprobado " + promedio;
+        respuestaGrande.textContent = "NO APROBADO";
+        respuestaGrande.className = "text-danger";
+        respuestaChiquita.textContent = "No aprobado con promedio: " + 60;
     }
 }
