6a81771bfa97bfc18da4d78744846bb8e36ab8de
GEORGINA RETA LIMAS <2430043@upv.edu.mx>
2025-10-27 19:37:25 -0600

    Ya muestra opciones

--- Practica_8/script.js
+++ Practica_8/script.js
@@ -12,6 +12,12 @@ function mostrarCantidadMaterias(){
     let cantidad = document.getElementById("cantidad_alumnos").value;
     console.log(cantidad);
 
+    if(cantidad <= 0){
+        Swal.fire('Error cantidad minima', 'La cantidad mínima de materias es 1','warning');
+        document.getElementById("cantidad_alumnos") = "";
+        return;
+    }
+
     const contenedor = document.getElementById('contenido');
     contenedor.innerHTML = '';
 
@@ -24,16 +30,16 @@ function mostrarCantidadMaterias(){
                     <h3 id="encabezado">Materia ${i+1}</h3>
                     <form id="formUnidades">
                         <label for="u1">Unidad 1</label>
-                        <input type="number" id="u1" class="form-control" min="0">
+                        <input type="number" id="u1" class="form-control">
 
                         <label for="u2">Unidad 2</label>
-                        <input type="number" id="u2" class="form-control" min="0">
+                        <input type="number" id="u2" class="form-control">
 
                         <label for="u3">Unidad 3</label>
-                        <input type="number" id="u3" class="form-control" min="0">
+                        <input type="number" id="u3" class="form-control">
 
                         <label for="u4">Unidad 4</label>
-                        <input type="number" id="u4" class="form-control" min="0">
+                        <input type="number" id="u4" class="form-control">
 
                         <button class="btn btn-primary rounded-pill px-3 mt-3" onclick="calificar('${i}')">
                             Calificar
@@ -54,32 +60,27 @@ function mostrarCantidadMaterias(){
     
 };
 
-/*
-document.getElementById("Materias").addEventListener("submit", function(e){ 
-    e.preventDefault();
-    
-    let m1 = document.getElementById("mat1").value;
-    let m2 = document.getElementById("mat2").value;
-    let m3 = document.getElementById("mat3").value;
-    let m4 = document.getElementById("mat4").value;
+function calificar(n){
+    const u1 = document.getElementById('u1').value;
+    const u2 = document.getElementById('u2').value;
+    const u3 = document.getElementById('u3').value;
+    const u4 = document.getElementById('u4').value;
 
-    let promedio;
-    let mensaje;
-    
-
-    if(m1 >= 70 && m2 >= 70 && m3 >= 70 && m4 >= 70){
-        promedio = (m1 + m2 + m3 + m4) / 4;
-        mensaje = "Aprobado: " + promedio;
-    } else {
-        promedio = 60;
-        mensaje = "Reprobado: " + promedio;
+    if(u1 <= 0 && u2 <= 0 && u3 <= 0 && u4 <= 0){
+        Swal.fire('Error cantidad minima', 'La cantidad mínima es 0','warning');
     }
 
+    if(u1 > 100 && u2 > 100 && u3 > 100 && u4 > 100){
+        Swal.fire('Error cantidad maxima', 'La cantidad maxima es 10','success');
+    }
 
-    console.log(mensaje);
-    document.getElementById("respuesta").value = mensaje;
-    alert(mensaje);
-});
+    let promedio = (u1 + u2 + u3 + u4)/4;
 
-
-*/
+    if(promedio >= 7){
+        document.getElementById('respuestaGrande') = "APROBADO";
+        let mensaje = "Aprobado " + promedio;
+    } else {
+        document.getElementById('respuestaGrande') = "REPROBADO";
+        let mensaje = "Reprobado " + promedio;
+    }
+}
